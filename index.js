fcd940ce6a8cc0c14fa1fc721ab61e05c0f5241f
Natasya3101 <natasyaalvngu@gmail.com>
2023-11-15 17:22:41 +0700

    tugas nd js

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import conn from "./database.js";
+import { addUser } from "./route/regist.js";
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.post("/api/register",addUser)
+app.listen(3000 , () =>{
+    // notification
+    console.log("The Server starts on port 3000");
+});
