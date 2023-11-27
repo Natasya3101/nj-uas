e0fbd3c799902609240c4314104911de07e858c8
Natasya3101 <natasyaalvngu@gmail.com>
2023-11-27 16:16:01 +0700

    gtw

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 import conn from "./database.js";
-import { addUser } from "./route/regist.js";
+import cookieParser from "cookie-parser";
+import { addUser, login } from "./route/regist.js";
 const app = express();
-app.use(express.static("public"));
+
+app.use(cookieParser());
 app.use(express.json());
-app.post("/api/register",addUser)
-app.listen(3000 , () =>{
+app.post("/api/register", addUser);
+
+app.use((req, res, next) => {
+    if (req.path.startsWith("/api/login") || req.path.startsWith("/api/register")) {
+        next();
+    } else {
+        let authorized = false;
+        if (req.cookies.token) { // ada ga si token itu di situ
+            try {
+                req.me = jwt.verify(req.cookies.token, "kim"); //kuncinya
+                authorized = true;
+            } catch (error) {
+                res.clearCookie("token"); // menghapus si cookienya
+            }
+        }
+        if (authorized) {
+            if (req.path.startsWith("/login")) {
+                res.redirect("/home");
+            }
+
+        } else {
+            if (req.path.startsWith("/login") || req.path.startsWith("/register")) {
+                next();
+            } else {
+                if (req.path.startsWith("/api")) {
+                    res.status(401);
+                    res.send("Anda Harus Login dulu");
+                } else {
+                    res.redirect("/login");
+                }
+            }
+        }
+    }
+});
+
+app.use(express.static("public"));
+app.post("/api/login", login);
+app.get("/api/logout",(req,res)=>{
+    res.clearCookie("token");
+    res.send("logout berhasil");
+})
+app.listen(3000, () => {
     // notification
     console.log("The Server starts on port 3000");
 });
--- route/regist.js
+++ route/regist.js
@@ -1,10 +1,20 @@
 import conn from "../database.js";
-
+import jwt  from "jsonwebtoken";
 export async function addUser(req, res) {
    const rows= await conn.query(
       `INSERT INTO account VALUES (NULL,'${req.body.username}','${req.body.password}')`
     );
-    res.send("User has been added");
-
-  
+    res.send("User has been added");  
+}
+export async function login(req,res){
+   const result = await conn.query(
+      `SELECT *FROM account where username = '${req.body.username}' && password = '${req.body.password}'`
+   )
+   if(result.length>0){
+      const token = jwt.sign({result},"kim");
+      res.cookie("token",token)
+      res.send({token});
+   }else{
+      res.send("login gagal");
+   }
 }
